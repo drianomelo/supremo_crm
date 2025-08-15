38a3a257df7fac440f3e0de06d200444caf3ed94
Adriano Melo <adrianosenny@gmail.com>
2025-08-15 20:58:05 -0300

    feat: add swiper functionality for image and video carousels

---
+++ js/swiper.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
---
+++ js/swiperVideo.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  direction: "horizontal",
+  loop: true,
+
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+});
